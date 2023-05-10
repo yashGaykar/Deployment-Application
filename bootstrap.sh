e82a9504cb56a847faacf8bb47010186881d559f
YashGaykar0309 <yash.gaykar@opcito.com>
2023-05-10 12:44:43 +0530

    updates: dockerfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,5 @@
 export FLASK_APP=./src/runcelery.py
 export FLASK_ENV=development
 export FLASK_DEBUG=1
+celery -A src.runcelery:celery1 worker -l info --concurrency=2 --pool=threads &
 flask run --host=0.0.0.0 --port=5000 
